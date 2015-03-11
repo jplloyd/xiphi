8ad3d2d512712a71276f8afabd62a059a48cd1ff
Jesper Lloyd <jpl.lloyd@gmail.com>
2015-03-11 07:11:51 +0100

    processing of chkprob which works but is horrific

--- CheckHub.hs
+++ CheckHub.hs
@@ -17,24 +17,31 @@ import Prelude hiding (log)
 -- constants (in order) and have to be fully type checked
 data ChkProb = ChkProb {constants :: [(N,SExpr)], term ::  SExpr, typ :: SExpr}
 
+-- would be easier to work with...
+data ChkProb' = ChkProb' [(N,Type)] SExpr Type
 
-processProb :: ChkProb -> Either Error (Term,Xi) 
+
+-- remaining
+-- - correctness of subst comp implementation
+-- - printing of substitutions
+-- - unless postulates & type have to be finalized, xi needs to be threaded between everything
+-- - options for printing different logs - store all logs up until possible errors
+
+processProb :: ChkProb -> Either Error (Term,Xi)
 processProb prob = do
   _Σ <- makeConstants (constants prob)
-  let (log1,_D') = scopecheck (typ prob)
+  let (sclog1,_D') = scopecheck (typ prob)
   _D <- _D'
-  let (log2,_T') = elaborate _Σ _D ISet
+  let (elablog1,_T') = elaborate _Σ _D ISet
   _T <- _T'
   if not (isFinal _T)
   then Left $ "Type checked against must be finalized: " ++ show _T
   else do
-  let (log3,e') = scopecheck (term prob)
+  let (sclog2,e') = scopecheck (term prob)
   e <- e'
-  let (log4,xi,term') = elaborate' _Σ e _T
+  let (elablog2,xi,term') = elaborate' _Σ e _T
   term <- term'
   return (term,xi)
-
-
 
 makeConstants :: [(N,SExpr)] -> Either Error Sigma
 makeConstants ls = case consts of
@@ -44,7 +51,7 @@ makeConstants ls = case consts of
         (logs,consts) = second sequence $ unzip . map scopecheck $ exprs
         go env [] = Right (Env env)
         go env ((n,e):cns) = case elaborate (Env env) e ISet of
-          (log, Right t) -> if isFinal t
-                            then go (env++[(n,t)]) cns
-                            else Left $ "postulates must be fully solved! \n" ++ show t
+          (log, Right t) -> {-if isFinal t
+                            then-} go (env++[(n,t)]) cns
+                            --else Left $ "postulates must be fully solved! \n" ++ show t
           (log,Left e') -> Left e'
