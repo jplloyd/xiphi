b9305de1a17375625521cccb18b16c6805086748
Marcus Johansson <marcuse.johansson@gmail.com>
2015-03-18 19:20:13 +0100

    Added solutions for updated Lam example.

--- examples/ALam.agda
+++ examples/ALam.agda
@@ -13,32 +13,31 @@ Id : Set1
 Id = {A : Set} → A → A
 
 -- Works because (λ x -> x) is made polymorphic using information from (eq Id)
-works : Set
-works = w (eq Id) (λ x -> x)
+linear : Set
+linear = w (eq Id) (λ x -> x)
 
 -- Fails because the (λ x -> x) cannot be made polymorphic after the fact
--- fails : Set
--- fails = f (λ x -> x) (eq Id)
-
-
+-- nonlin : Set
+-- nonlin = f (λ x -> x) (eq Id)
 
 
 -- An additional observation:
 
--- Polymorphic identity function
-id : {A : Set} → A → A
+-- Typed polymorphic identity function
+id : Id
 id = λ {A : Set} (a : A) → a
 
--- Works since 
-ws : Set
---ws = w (eq Id) id
-ws = w (eq Id) (id {_})
+-- Works since hidden lambda is inserted, as well as hidden argument
+lin : Set
+lin = w (eq Id) id
+-- lin = w (eq Id) (id {_})
+-- lin = w (eq Id) (λ {A} -> id {_})
 
--- Interestingly, fs fails because of a different, but possibly related issue,
+-- Interestingly, the following because of a different, but possibly related issue,
 -- namely that a meta is unnecessarily inserted for the implicit argument of id.
--- fs : Set
--- fs = f  id      (eq Id)
--- fs = f (id {_}) (eq Id)
+-- non : Set
+-- non = f  id      (eq Id)
+-- non = f (id {_}) (eq Id)
 -- This indicates a discrepancy where hidden arguments are inserted more eagerly
--- than hidden lambdas: fs type checks when the hidden lambda is inserted as well.
--- fs = f (λ {_} -> id {_}) (eq Id)
+-- than hidden lambdas: it type checks a dummy hidden lambda is inserted as well.
+-- non = f (λ {A} -> id {_}) (eq Id)
--- examples/HLam.hs
+++ examples/HLam.hs
@@ -1,41 +1,39 @@
-module HLam (works,fails) where
+module HLam where
 
 import Surface
 import CheckHub
 
-eBool = fun dSet (fun dSet SSet)
-etrue = elam2 _x _y vx
-
-epoly = funASet dA vA
-emono = fun dSet SSet
-
 -- Postulates
 postulates =
-  [(_Eq, funASet dA SSet)
-  ,(_eq, funBSet baB (appEq vB va))
-  ,(_w, funbBool dappEq_vb (fun difb SSet))
-  ,(_f, funbBool difb (fun dappEq_vb SSet))
+  [(_Eq, fun dSet SSet)
+  ,(_eq, fun bTSet (appEq vT))
+  
+  ,(_w, funTSet dEq_T (fun dT    SSet))
+  ,(_f, funTSet dT    (fun dEq_T SSet))
   ]
+
+-- Polymorphic id type
+eId = funASet dA vA
+-- Monomorphic id which is to be turned polymorphic
+eid = elam1 _x vx
 
 -- Type checking problems
 simple n = ChkProb (take n postulates) SSet SSet
-works = chkSet (eapp2 cw eq_true eid)
-fails = chkSet (eapp2 cf eid eq_true)
+
+linear = chkSet (eapp2 cw eq_Id eid  )
+nonlin = chkSet (eapp2 cf eid   eq_Id)
 -- where
 chkSet e = ChkProb postulates e SSet
 
 -- Identifiers
 _Eq = "Eq"
 _eq = "eq"
+_T = "T"
 _A = "A"
-_B = "B"
 _a = "a"
 _w = "w"
 _f = "f"
-_b = "b"
 _x = "x"
-_y = "y"
-_z = "z"
 
 -- Constants
 cEq = SCns _Eq
@@ -44,31 +42,24 @@ cw = SCns _w
 cf = SCns _f
 
 -- Variables
+vT = SVar _T
 vA = SVar _A
-vB = SVar _B
 va = SVar _a
 vx = SVar _x
-vz = SVar _z
-vb = SVar _b
 
 -- Bindings
+bTSet = SBind _T SSet
 bASet = SBind _A SSet
-bBSet = SBind _B SSet
 baA = SBind _a vA
-baB = SBind _a vB
-bbBool = SBind _b eBool
 
 -- Dummy bindings
 dSet = dummy SSet
+dT = dummy vT
 dA = dummy vA
-dappEq_vb = dummy (appEq eBool vb)
-difb = dummy (eapp2 vb epoly emono)
-
--- Function abstractions
-eid = elam1 _z vz
+dEq_T = dummy (appEq vT)
 
 -- Applications
-eq_true = SApp ceq [SPos eBool] etrue
+eq_Id = eapp1 ceq eId
 
 -- Convenience functions
 eapp1 fe ae1 = SApp fe [] ae1
@@ -77,11 +68,10 @@ eapp2 fe ae1 ae2 = eapp1 (eapp1 fe ae1) ae2
 elam1 n1 e = SLam [] n1 e
 elam2 n1 n2 e = elam1 n1 (elam1 n2 e)
 
-appEq impl = SApp cEq [SPos impl]
+appEq = eapp1 cEq
 dummy = SBind "£"
 
+funTSet = SFun [bTSet]
 funASet = SFun [bASet]
-funbBool = SFun [bbBool]
 fun = SFun []
 
-funBSet = SFun [bBSet]
